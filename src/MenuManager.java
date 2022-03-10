190fc146c9774cb1a2f75e270da67e4b539ccb9f
gnuyong <djawpdyd01@naver.com>
2022-03-10 21:06:43 +0900

    added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,14 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1.Add fitness center members");
+		System.out.println("2.Delete fitness center members");
+		System.out.println("3.Edit fitness center members");
+		System.out.println("4.View fitness center members");
+		System.out.println("5.Exit");
+		
+
+	}
+
+}
